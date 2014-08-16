d19deebd9c54d9488bbd90c842da455fefc0151d
Alina Solovjova <alina@gocardless.com>
2014-08-16 13:39:09 +0100

    Fixing case sensitive search in Heroku

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -9,7 +9,7 @@ class Ride < ActiveRecord::Base
 	has_and_belongs_to_many :users
 
 	def self.search_for(query)
-		where('name LIKE :query OR description LIKE :query',
+		where('name ILIKE :query OR description ILIKE :query',
 			query: "%#{query}%")
 	end
 
